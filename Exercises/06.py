58816599a1e554a23e44c7d096bb05e01a2096a9
Gabriel Santos de Sousa <gabsousa.contato@gmail.com>
2022-02-14 23:10:58 -0300

    feat: add 06

---
+++ Exercises/06.py
@@ -0,0 +1,8 @@
+#Exercise 06
+print('<<Velocity Calculator>>');
+print();
+space = float(input('Enter the distance of your trip(km): '));
+print();
+speed = float(input('Enter the average speed of your car(km/h): '));
+print();
+print(f'RESULT: You will take {(space/speed):.2f} hours');
