c6bc59021afd780815060e263085527034a739aa
Gabriel Santos de Sousa <gabsousa.contato@gmail.com>
2022-02-14 22:44:10 -0300

    feat: add 05

---
+++ Exercises/05.py
@@ -0,0 +1,9 @@
+#Exercise 05
+print('<<Supermarket Python for Zombies>>');
+print();
+price = float(input('Enter the product price: '));
+print();
+percent = int(input('Enter the discount: '));
+discount = (percent * price) / 100;
+print();
+print(f'RESULT: With the {percent}%(-{discount:.2f}) discount, the product now costs {(price - discount):.2f}');
