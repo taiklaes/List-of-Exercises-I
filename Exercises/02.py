666ffa5dbf890be436d349ddb110d26c07b52642
Gabriel Santos de Sousa <gabsousa.contato@gmail.com>
2022-02-14 22:10:29 -0300

    feat: add two decimal numbers

--- Exercises/02.py
+++ Exercises/02.py
@@ -3,4 +3,4 @@ print('<<Meter to Millimeter Converter>>');
 print();
 m = float(input('Enter the value in meters: '));
 print();
-print(f'RESULT: {m} meter to {m * 1000} millimeter');
+print(f'RESULT: {m:.2f} meter to {m * 1000} millimeter');
