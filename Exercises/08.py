125acce19dff59c3e5b414a4da687b4c72911870
Gabriel Santos de Sousa <gabsousa.contato@gmail.com>
2022-02-15 15:23:24 -0300

    feat: add 08

---
+++ Exercises/08.py
@@ -0,0 +1,6 @@
+#Exercise 08
+print('<<Fahrenheit to Celsius Converter>>');
+print();
+f = float(input('Enter the value in degrees: '));
+print();
+print(f'RESULT: {f:.1f}°F to {((f - 32) * 5 / 9):.1f}°C');
