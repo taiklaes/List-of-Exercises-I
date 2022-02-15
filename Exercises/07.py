5d8f0acba5732c2ffd40cce51e92dee17c3b6af7
Gabriel Santos de Sousa <gabsousa.contato@gmail.com>
2022-02-14 23:16:07 -0300

    feat: add 07

---
+++ Exercises/07.py
@@ -0,0 +1,6 @@
+#Exercise 07
+print('<<Celsius to Fahrenheit Converter>>');
+print();
+c = float(input('Enter the value in degrees: '));
+print();
+print(f'RESULT: {c:.2f}°C to {((9 * c)/5) + 32}°F');
