18194e1ac5d2b2bf67fc28688d4cbe9f605d9be3
Gabriel Santos de Sousa <gabsousa.contato@gmail.com>
2022-02-18 18:08:32 -0300

    feat: add 09

---
+++ Exercises/09.py
@@ -0,0 +1,10 @@
+#Exercise 09
+print('<<Rental Car Python for Zombies>>');
+print();
+km = float(input('What is the number of km traveled by your rental car: '));
+km = km * 0.15;
+print();
+days = float(input('How many days was your car rented: '));
+days = days * 60;
+print();
+print(f'RESULT: You must pay R${(days + km):.2f}');
