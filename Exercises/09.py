79cd9d686eedad002ec15d553816c93595b8d12c
Gabriel Santos de Sousa <gabsousa.contato@gmail.com>
2022-02-21 16:49:13 -0300

    feat: replace float to int

--- Exercises/09.py
+++ Exercises/09.py
@@ -4,7 +4,7 @@ print();
 km = float(input('What is the number of km traveled by your rental car: '));
 km = km * 0.15;
 print();
-days = float(input('How many days was your car rented: '));
+days = int(input('How many days was your car rented: '));
 days = days * 60;
 print();
 print(f'RESULT: You must pay R${(days + km):.2f}');
--- Exercises/10.py
+++ Exercises/10.py
@@ -4,7 +4,7 @@ print();
 cigarettes = int(input('How many cigarettes do you smoke a day: '));
 lost = cigarettes * 10;
 print();
-years = float(input('How many years have you been smoking: '));
+years = int(input('How many years have you been smoking: '));
 days = years * 365;
 print();
 print(f'You lost {int((lost * days) / 1440)} days of life');
